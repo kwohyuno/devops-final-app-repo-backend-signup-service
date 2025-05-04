6c956deb78e9e144f5f6d67985e1a21455126f50
kwohyuno <121947555+kwohyuno@users.noreply.github.com>
2025-05-04 00:04:46 -0700

    api url

--- src/main/java/com/example/SpringDemo/Controller/MemberController.java
+++ src/main/java/com/example/SpringDemo/Controller/MemberController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api2/members")
+@RequestMapping("/members")
 @CrossOrigin
 public class MemberController {
     private final MemberService memberService;
